f75626e56bb8c369060801ae888a8ad19b2576fc
Oleg Goncharov <56592498+OlegGoncharovAdrg@users.noreply.github.com>
2019-11-25 15:13:38 +0300

    Initial commit

---
+++ Sanitize/Delete-data/Form1.Designer.cs
@@ -0,0 +1,167 @@
+﻿namespace Delete_data
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.button3 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(0, 0);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(281, 284);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(6, 19);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(120, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Запуск поиска приборов";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(6, 105);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(219, 95);
+            this.listBox1.TabIndex = 3;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.listBox1);
+            this.groupBox1.Controls.Add(this.button1);
+            this.groupBox1.Location = new System.Drawing.Point(289, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(233, 272);
+            this.groupBox1.TabIndex = 4;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Меню поиска";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(7, 86);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(153, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Выберите доступный прибор";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.button2);
+            this.groupBox2.Controls.Add(this.richTextBox1);
+            this.groupBox2.Location = new System.Drawing.Point(528, 12);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(260, 272);
+            this.groupBox2.TabIndex = 5;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Тест соединения";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(7, 19);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Запустить тест";
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(6, 54);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(247, 115);
+            this.richTextBox1.TabIndex = 0;
+            this.richTextBox1.Text = "";
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(299, 302);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(489, 23);
+            this.button3.TabIndex = 6;
+            this.button3.Text = "Запуск программной очистки";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.ClientSize = new System.Drawing.Size(800, 340);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.pictureBox1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Мастер очистки памяти";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Button button3;
+    }
+}
+
---
+++ Sanitize/Delete-data/Form1.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Ivi.Visa;
+using Ivi.Visa.FormattedIO;
+
+namespace Delete_data
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            listBox1.Hide();
+            groupBox2.Hide();
+            button3.Hide();
+            label1.Hide();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            label1.Show();
+            listBox1.Show();
+            IEnumerable<string> devices;
+            try
+            {
+                
+                devices = GlobalResourceManager.Find();
+                var every2ndElement = devices.Where((p, index) => index % 2 == 0);
+                foreach (string device in every2ndElement)
+                {
+                    listBox1.Items.Add(device);
+                }
+                this.listBox1.MouseClick += new MouseEventHandler(listBox1_MouseClick);
+            }
+            catch (VisaException ex)
+            {
+                MessageBox.Show("Инструментов не найдено");
+            }
+        }
+
+
+        void listBox1_MouseClick(object sender, MouseEventArgs e)
+        {
+            groupBox2.Show();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            string VISA_ADDRESS = listBox1.GetItemText(listBox1.SelectedItem);
+            // Create a connection (session) to the instrument
+            IMessageBasedSession session;
+            session = GlobalResourceManager.Open(VISA_ADDRESS) as IMessageBasedSession;
+      
+            // Create a formatted I/O object which will help us format the data we want to send/receive to/from the instrument
+            MessageBasedFormattedIO formattedIO = new MessageBasedFormattedIO(session);
+
+            // For Serial and TCP/IP socket connections enable the read Termination Character, or read's will timeout
+            if (session.ResourceName.Contains("ASRL") || session.ResourceName.Contains("SOCKET"))
+                session.TerminationCharacterEnabled = true;
+
+            formattedIO.WriteLine(":SYSTem:SECurity:ERASeall");
+            formattedIO.WriteLine(":SYSTem:SECurity:SANitize");
+            formattedIO.WriteLine(":SYSTem:FILesystem:STORage:FSDCard ON|OFF|1|0");
+            formattedIO.WriteLine(":SYSTem:PRESet: PERSistent");
+            formattedIO.WriteLine(":SYSTem:COMMunicate:LAN:DEFaults");            formattedIO.WriteLine(":CAL:IQ:DEF");                                        
+            session.Dispose();
+            MessageBox.Show("Очистка заврешена");
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            string ac = listBox1.SelectedItem.ToString();
+            GetVisaChecked(ac);
+            button3.Show();
+        }
+
+        private void GetVisaChecked(string visa_addr)
+        {
+            IMessageBasedSession session;
+            try
+            {
+                session = GlobalResourceManager.Open(visa_addr) as IMessageBasedSession;
+                richTextBox1.Text = "Соединение установлено";
+            }
+            catch (NativeVisaException visaException)
+            {
+                richTextBox1.Text = "Соединение не установлено";
+            }
+        }
+        
+    }
+}
